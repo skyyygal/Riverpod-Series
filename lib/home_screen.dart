af53ae8d9f91b190a5813238d4ee9b83753aea5d
skyyygal <heeramnakp@gmail.com>
2025-06-21 18:12:34 +0530

    Intializing the riverpod

---
+++ lib/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// TO READ CONST VALUE
+final hello = Provider<String>((ref) {
+  return 'Subscriber';
+});
+
+final age = Provider<int>((ref) {
+  return 24;
+});
+
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(child: Text("Hello")),
+    );
+  }
+}
